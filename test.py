79a56c496e180c6bf9f3dd30ebee43a60a8b662b
ssynn <824063458@qq.com>
2019-06-11 11:19:14 +0800

    删除一些注释

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import src.main_widget as mw
-from PyQt5.QtWidgets import QApplication
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = mw.MainWindow()
-    ex.show()
-    sys.exit(app.exec_())
